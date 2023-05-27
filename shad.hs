05b70f99fd0eec4f520ce744311baae2e1e676a8
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-26 22:30:06 -0600

    Added function returning the modulus of 2

--- shad.hs
+++ shad.hs
@@ -7,4 +7,8 @@ import Foreign.C.Types
 solid :: CInt -> CInt
 solid i = 1
 
+check :: CInt -> CInt
+check i = mod i 2
+
 foreign export ccall solid :: CInt -> CInt
+foreign export ccall check :: CInt -> CInt
