8c59c96cc6064f72dbf24b72ccc3b66070d74044
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-27 02:09:24 -0600

    Updated checkerboard shader to take size, X and Y coordinates are provided via arguments and aren't calculated given a single integral index and the stride

--- shad.hs
+++ shad.hs
@@ -3,12 +3,13 @@
 module Shad where
 
 import Foreign.C.Types
+import Data.Bits
 
 solid :: CInt -> CInt
 solid i = 1
 
-check :: CInt -> CInt
-check i = mod i 2
+check :: CInt -> CInt -> CInt -> CInt
+check x y stride = toEnum $ fromEnum $ xor (mod x (stride * 2) > stride) (mod y (stride * 2) > stride)
 
 foreign export ccall solid :: CInt -> CInt
-foreign export ccall check :: CInt -> CInt
+foreign export ccall check :: CInt -> CInt -> CInt -> CInt
