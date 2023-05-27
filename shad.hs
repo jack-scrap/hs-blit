a519acc738d42def287075d072f8aa6475880148
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-27 02:15:55 -0600

    Added function to plot rectangle

--- shad.hs
+++ shad.hs
@@ -14,6 +14,10 @@ stripe x stride = toEnum $ fromEnum $ mod x (stride * 2) > stride
 check :: CInt -> CInt -> CInt -> CInt
 check x y stride = toEnum $ fromEnum $ xor (mod x (stride * 2) > stride) (mod y (stride * 2) > stride)
 
+rect :: CInt -> CInt -> CInt -> CInt -> CInt -> CInt -> CInt
+rect x y startX startY wd ht = toEnum $ fromEnum $ x >= startX && x <= startX + wd && y >= startY && y <= startY + ht
+
 foreign export ccall solid :: CInt -> CInt
 foreign export ccall stripe :: CInt -> CInt -> CInt
 foreign export ccall check :: CInt -> CInt -> CInt -> CInt
+foreign export ccall rect :: CInt -> CInt -> CInt -> CInt -> CInt -> CInt -> CInt
