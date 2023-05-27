e2c1ad52734d3c41a9bd74b042a7e8248dbd258f
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-27 02:48:15 -0600

    Added shader displaying a right-angle triangle at a perfect 90 degree angle

--- shad.hs
+++ shad.hs
@@ -17,7 +17,11 @@ check x y stride = toEnum $ fromEnum $ xor (mod x (stride * 2) > stride) (mod y 
 rect :: CInt -> CInt -> CInt -> CInt -> CInt -> CInt -> CInt
 rect x y posX posY wd ht = toEnum $ fromEnum $ x >= posX && x <= posX + wd && y >= posY && y <= posY + ht
 
+rightTri :: CInt -> CInt -> CInt
+rightTri x y = toEnum $ fromEnum $ x < y
+
 foreign export ccall solid :: CInt -> CInt
 foreign export ccall stripe :: CInt -> CInt -> CInt
 foreign export ccall check :: CInt -> CInt -> CInt -> CInt
 foreign export ccall rect :: CInt -> CInt -> CInt -> CInt -> CInt -> CInt -> CInt
+foreign export ccall rightTri :: CInt -> CInt -> CInt
