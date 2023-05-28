7bd1f327de5f60e14d8c1a74729c5450cf326d18
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-27 23:55:15 -0600

    Formatted

--- shad.hs
+++ shad.hs
@@ -21,7 +21,7 @@ rect :: CInt -> CInt -> CInt -> CInt -> CInt -> CInt -> CInt
 rect x y posX posY wd ht = toEnum $ fromEnum $ inRng x posX (posX + wd) && inRng y posY (posY + ht)
 
 border :: CInt -> CInt -> CInt -> CInt -> CInt -> CInt
-border x y wd ht thick = toEnum $ fromEnum $ not (inRng x thick (wd - thick)) || not (inRng y thick (ht - thick))
+border x y wd ht stroke = toEnum $ fromEnum $ not (inRng x stroke (wd - stroke)) || not (inRng y stroke (ht - stroke))
 
 prime :: CInt -> CInt
 prime n = toEnum $ fromEnum $ if n > 1
